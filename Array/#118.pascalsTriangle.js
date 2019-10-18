b4fbf67ca3015e2eecd40ea4ef993b75eaa74654
hibow <utfnalin@gmail.com>
2019-10-18 14:55:14 -0500

    added #118 to array

---
+++ Array/#118.pascalsTriangle.js
@@ -0,0 +1,55 @@
+/**
+ * Given a non-negative integer numRows, generate the first numRows of Pascal's triangle.
+ * In Pascal's triangle, each number is the sum of the two numbers directly above it.
+ *
+ *
+ *   [  [1], [1, 1], [1, 2, 1]]
+ *   1            default [[1]]   counter 1
+ *   1 1          iterate over [1] ->  push 1, check next element if exist and add onto 1, push to array; if not, push 1
+ *   1 2 1
+ *   1 3 3 1
+ *   1 4 6 4 1
+ *   1 5 10 10 5 1
+ */
+
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+  //input: number
+  //output: an array of multiple arrays based on input
+  //edge case: if input is null or 0, then return []
+  //constraints: O(n^2)
+  //initialize an array [] and push [1] as default and an counter 1
+  let result = [];
+  if (!numRows) {
+    return result;
+  }
+  let counter = 1;
+  //repeat the following until count is equal to numRows
+  while (counter <= numRows) {
+    //check the previous element, iterate over it
+    let newArr = [];
+    //push 1 to new array
+    newArr.push(1);
+    //if other element exist, add to previous element and push it to new array
+    if (result.length && counter > 2) {
+      let preElm = result[counter - 2];
+      for (let i = 1; i < preElm.length; i++) {
+        newArr.push(preElm[i] + preElm[i - 1]);
+      }
+    }
+    //push 1 to new array
+    if (counter > 1) {
+      newArr.push(1);
+    }
+    result.push(newArr);
+    //push new array to result array
+    counter++;
+  }
+  return result;
+};
+
+var a = generate(5);
+console.log(JSON.stringify(a));
--- README.md
+++ README.md
@@ -5,8 +5,11 @@ Leetcode challenges in javascript
 ## Table of Contents
 
 - Array
+
   - #1 two Sum
   - #11 max Area
   - #31 next Permutation
   - #88 merge Sort Array
+  - #118 pascalsTriangle.js
+
 - LinkedList
