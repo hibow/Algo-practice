27fdb74ef1688a77b759311adf70474c900ca41b
hibow <utfnalin@gmail.com>
2019-10-20 16:29:25 -0500

    added #108 to array

--- Array/#108.sortedArrayToBST.js
+++ Array/#108.sortedArrayToBST.js
@@ -9,9 +9,49 @@ Given the sorted array: [-10,-3,0,5,9],
 
 One possible answer is: [0,-3,9,-10,null,5], which represents the following height balanced BST:
 
-      0
+      0              assign middle as root
      / \
-   -3   9
+   -3   9            assign next middle point to left and right
    /   /
- -10  5
+ -10  5              repeat until no value
  */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+  function TreeNode(val) {
+    this.val = val;
+    this.left = this.right = null;
+  }
+var sortedArrayToBST = function(nums, start, end) {
+  if (start === undefined && end === undefined ) {
+    start = 0;
+    end = nums.length -1;
+  }
+  //find the middle point
+  if (start > end) {
+    return null;
+  }
+  let mid = Math.ceil((start + end) /2);
+  //assign to root
+  let root = new TreeNode(nums[mid]);
+  //assign to left
+  root.left = sortedArrayToBST(nums, start, mid - 1);
+  //assign to right
+  root.right = sortedArrayToBST(nums, mid + 1, end);
+  return root;
+};
+/**
+ * test case
+ */
+
+ var arr = [-10,-3,0,5,9];
+ var a = sortedArrayToBST(arr);
+ console.log(JSON.stringify(a));
