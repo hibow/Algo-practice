0f82aaa3019d07f351045e459ebc9bfeee466719
hibow <utfnalin@gmail.com>
2019-10-21 19:42:39 -0500

    modified #2

--- LinkedList/2. Add two Numbers/add-two-numbers.js
+++ LinkedList/2. Add two Numbers/add-two-numbers.js
@@ -11,7 +11,8 @@ Example:
 Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
 Output: 7 -> 0 -> 8
 Explanation: 342 + 465 = 807.
-  
+
+#1
   1)convert linkted list into number:
   push node value into an array  2 4  3  => 2 4 3 
   revert the array 3 4 2 
@@ -30,6 +31,13 @@ add each value to the linked list and convert to number
   convert number into array => 7 0 8
   convert array into linked list =>
    807 => 8 0 7 => 7 -> 0 -> 8
+#2
+
+iterate over both lists
+add node values together
+if sum is greater than 10 
+save the remainder of sum / 10 and add it to next node
+  if next node is null, add it to 0 and add to the list
 
 
  * 
@@ -46,37 +54,35 @@ add each value to the linked list and convert to number
  * @return {ListNode}
  */
 
-// import LinkedList from "../linkedList.js";
 function ListNode(val) {
   this.val = val;
   this.next = null;
 }
 
 var addTwoNumbers = function(l1, l2) {
-  const listToNum = function(list) {
-    let str = "";
-    //if list next exist, repeat the loop
-    while (list.val) {
-      let rootStr = list.val.toString();
-      str = rootStr + str;
-      //if list value exist, add into string
-    }
-    return Number(str);
-  };
-  let sum = listToNum(l1) + listToNum(l2);
-  console.log(sum);
-  const numToList = function(num) {
-    numStr = num.toString();
-    let n = numStr.length - 1;
-    let result = new ListNode(Number(numStr[n]));
-    n--;
-    let curr = result;
-    while (numStr[n]) {
-      curr.next = new ListNode(Number(numStr[n]));
-      curr = curr.next;
-      n--;
-    }
-    return result;
-  };
-  return numToList;
+  //output: new list
+  //edge case: different length between l1 and l2, 
+  //           one is empty list(X)
+  let newList = new ListNode(0); //return newList.next
+  let curNewLst = newList;  //use it as current pointer
+  let curL1 = l1;  //l1 pointer
+  let curL2 = l2;  //l2 pointer
+  let carry = 0;
+// iterate over both lists
+  while(curL1 !== null || curL2 !== null) {
+    // add node values together
+    let sum = (( curL1 !== null) ? curL1.val : 0) + ( (curL2 !== null) ? curL2.val : 0) + carry;
+    carry = Math.floor(sum / 10);
+    curNewLst.next = new ListNode( Math.floor(sum % 10));
+    curNewLst = curNewLst.next;
+    if (curL1 !== null) curL1 = curL1.next;
+    if (curL2 !== null) curL2 = curL2.next;
+}
+// if sum is greater than 10 => if carry > 0
+// save the remainder of sum / 10 and add it to next node
+//   if next node is null, add it to 0 and add to the list
+  if (carry > 0) {
+    curNewLst.next = new ListNode(carry);
+  }
+  return newList.next;
 };
