c92cc68e814135a8701cadc5ec7ef9ab459dfffd
hibow <utfnalin@gmail.com>
2019-10-23 20:48:30 -0500

    added automated python script to create dir and files

---
+++ leetcodes/1. Two Sum/twoSum.js
@@ -0,0 +1,58 @@
+/*
+ * [Source]
+ *
+ * https://leetcode.com/problems/two-sum/
+ *
+ * [Problem Description]
+ *
+ * Given an array of integers, return indices of the two numbers such that they
+ * add up to a specific target.
+ * You may assume that each input would have exactly one solution, and you may not
+ * use the same element twice.
+ * Example:
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ *
+ * [Comments]
+ *
+ *
+ *
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+  //input: an array of numbers, a number
+  //output: an array of numbers (two numbers)
+  //constraints: O(n^2), only one solution
+  //edge case: no null case
+
+  //brute force O(n^2)
+  //iterate over the array
+  // for (let i = 0; i < nums.length; i++) {
+  //   //for each number (index = i)
+  //   //iterate over the array except itself (index = j , i doesn't equal to j)
+  //   for (let j = i + 1; j < nums.length; j++) {
+  //     //add the current number and check if it equals to target
+  //     if (nums[i] + nums[j] === target) {
+  //       //if yes, return [i, j]
+  //       return [i, j];
+  //     }
+  //   }
+  // }
+  //one pass hash table: O(n)
+  let obj = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    let remain = target - nums[i];
+    if (obj[remain] !== undefined && obj[remain] !== i) {
+      return [obj[target - nums[i]], i];
+    }
+    obj[nums[i]] = i;
+  }
+};
+
---
+++ leetcodes/19. Remove Nth Node From End of List/removeNthNodeFromEndofList.js
@@ -0,0 +1,61 @@
+/*
+ * [Source]
+ *
+ * https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+ *
+ * [Problem Description]
+ *
+ * Given a linked list, remove the n-th node from the end of list and return its
+ * head.
+ * Example:
+ * Given linked list: 1->2->3->4->5, and n = 2.
+ * After removing the second node from the end, the linked list becomes 1->2->3->5.
+ * Note:
+ * Given n will always be valid.
+ * Follow up:
+ * Could you do this in one pass?
+ *
+ * [Comments]
+ *
+ *
+ *
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function(head, n) {
+  //input: list and number
+  //output: list
+  //one pass and two pass -> iteration times O(n) vs O(2n)
+  let newHead = new ListNode(0); //return newHead.next in case only one node in list [1]
+  newHead.next = head;
+  let fstPt = newHead;
+  let secPt = newHead;
+  //create a gap between fstPt and secPt with n
+  //when fstPt run to the finish line, secPt will be at the position before the nth node.
+  //secPt will skip nth node and point its next to next next node
+  //return newHead.next
+  let count = n;
+  while (fstPt.val) {
+    if (count > 0) {
+      fstPt = fstPt.next;
+      count--;
+    } else if (count === 0) {
+      fstPt = fstPt.next;
+      secPt = secPt.next;
+    }
+  }
+  secPt.next = secPt.next.next;
+
+  return newHead.next;
+};
+
+
