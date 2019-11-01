163939119b6cec8a4ee018515034b3a32ad9f3fc
hibow <utfnalin@gmail.com>
2019-11-01 17:00:23 -0500

    updated folder structure

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  clearMocks: true,
+  watchPathIgnorePatterns: ["node_modules"],
+  testEnvironment: "node",
+  testMatch: ["**/__tests__/**/*.js?(x)", "**/?(*.)+(spec|test).js?(x)"],
+  verbose: false
+};
--- leetcodes/1. Two Sum/twoSum.test.js
+++ leetcodes/1. Two Sum/twoSum.test.js
@@ -2,32 +2,33 @@ const twoSum = require("./twoSum.js");
 const file = require.resolve("./twoSum.py");
 const { spawn } = require("child_process");
 // const pyFunc = spawn("python", ["./twoSum.py"]);
-let test1Args = [[2, 7, 3, 5], 9];
-let args = [file].concat(test1Args);
-console.log(args);
-const pyFunc = spawn("python", args);
-pyFunc.stdout.setEncoding("utf8");
-pyFunc.stdout.on("data", function(data) {
-  console.log("data:", data);
-});
-pyFunc.stderr.on("data", function(data) {
-  console.error(data.toString());
-});
-// describe("Two Sum", () => {
-//   test("Two or more elements", () => {
-//     expect(twoSum([2, 7, 3, 5], 9)).toStrictEqual([0, 1]);
-//     expect(twoSum([3, 2, 4], 6)).toStrictEqual([1, 2]);
-//   });
-// });
 
-// describe("Two Sum in Python", () => {
-//   test("Two or more elements", () => {
-//     let test1Args = [[2, 7, 3, 5], 9];
-//     let args = ["./twoSum.py"].concat(test1Args);
-//     const pyFunc = spawn("python", args);
-//     pyFunc.stdout.on("data", function(data) {
-//       console.log("data:", JSON.stringify(data));
-//       expect(data).toStrictEqual([0, 1]);
-//     });
-//   });
-// });
+describe("Two Sum", () => {
+  test("Two or more elements", done => {
+    expect(twoSum([2, 7, 3, 5], 9)).toStrictEqual([0, 1]);
+    expect(twoSum([3, 2, 4], 6)).toStrictEqual([1, 2]);
+    done();
+  });
+});
+
+describe("Two Sum in Python", () => {
+  test("Two or more elements", async () => {
+    try {
+      let test1Args = [[2, 7, 3, 5], 9];
+      let args = [file].concat(test1Args);
+      const pyFunc = await spawn("python", args);
+      await pyFunc.stdout.setEncoding("utf8");
+      let output;
+      await pyFunc.stdout.on("data", async function(data) {
+        await console.log("data:", data);
+        output = data;
+        console.log("what here2?");
+        // expect(data).resolves.toStrictEqual([0, 1]);
+      });
+      console.log(await output);
+      await expect(output).toStrictEqual([0, 1]);
+    } catch (err) {
+      await console.log(err);
+    }
+  });
+});
