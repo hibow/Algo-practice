fc14f88d8550fea3e206ae7de82e97c4b7c6dcc8
hibow <utfnalin@gmail.com>
2019-11-05 18:05:06 -0600

    added #136

--- leetcodes/136. Single Number/singleNumber.js
+++ leetcodes/136. Single Number/singleNumber.js
@@ -1,10 +1,10 @@
-/*          
- * [Source] 
- *          
+/*
+ * [Source]
+ *
  * https://leetcode.com/problems/single-number/
- *          
+ *
  * [Problem Description]
- *          
+ *
  * Given a non-empty array of integers, every element appears twice except for
  * one. Find that single one.
  * Note:
@@ -16,11 +16,24 @@
  * Example 2:
  * Input: [4,1,2,1,2]
  * Output: 4
- *          
+ *
  * [Comments]
- *          
- *          
- *          
- */          
+ *
+ *    no extra memory -> constant space -> bit manipulation
+ *      a xor 0 = a
+ *      a xor a = 0
+ *      a xor a xor b = b
+ */
 
-
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+  //constraints: space O(1), time O(n)
+  let a = 0;
+  for (let i = 0; i < nums.length; i++) {
+    a ^= nums[i];
+  }
+  return a;
+};
