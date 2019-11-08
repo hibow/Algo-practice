74a723e9bb9ac8740e597691a5462622058a8538
hibow <utfnalin@gmail.com>
2019-11-08 17:41:38 -0600

    added #242

--- leetcodes/242. Valid Anagram/validAnagram.js
+++ leetcodes/242. Valid Anagram/validAnagram.js
@@ -1,10 +1,10 @@
-/*          
- * [Source] 
- *          
+/*
+ * [Source]
+ *
  * https://leetcode.com/problems/valid-anagram/
- *          
+ *
  * [Problem Description]
- *          
+ *
  * Given two strings s and t , write a function to determine if t is an anagram of
  * s.
  * Example 1:
@@ -18,11 +18,29 @@
  * Follow up:
  * What if the inputs contain unicode characters? How would you adapt your
  * solution to such case?
- *          
+ *
  * [Comments]
- *          
- *          
- *          
- */          
+ *
+ *
+ *
+ */
 
-
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+  //edge case : if s len not equals t len return false
+  //hash map or array for character's index
+  if (t.length !== s.length) return false;
+  const dict = {};
+  for (let c of s) {
+    dict[c] = (dict[c] || 0) + 1;
+  }
+  for (let c of t) {
+    if (!dict[c]) return false;
+    dict[c]--;
+  }
+  return true;
+};
