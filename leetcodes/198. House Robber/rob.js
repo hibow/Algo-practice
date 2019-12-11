bde2e85311e0ee7630c296d60b9e8ebbf5c05737
hibow <utfnalin@gmail.com>
2019-12-11 15:51:08 -0600

    added #198

---
+++ leetcodes/198. House Robber/rob.js
@@ -0,0 +1,40 @@
+/**
+ * 
+ * You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
+
+Example 1:
+
+Input: [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+             Total amount you can rob = 1 + 3 = 4.
+Example 2:
+
+Input: [2,7,9,3,1]
+Output: 12
+Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+             Total amount you can rob = 2 + 9 + 1 = 12.
+
+             1- 3 5 
+             2 4 6...
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+  if (nums.length === 1) {
+    return nums[0];
+  }
+
+  let DP = [];
+  DP[0] = 0;
+  DP[1] = nums[0];
+  for (let i = 2; i <= nums.length; i++) {
+    DP[i] = Math.max(DP[i - 1], DP[i - 2] + nums[i - 1]);
+  }
+
+  return DP[nums.length];
+};
