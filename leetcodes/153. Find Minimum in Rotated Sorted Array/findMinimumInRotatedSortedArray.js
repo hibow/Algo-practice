d39d9713ff7f09a7fe80a6bf08677e92acb924fd
hibow <utfnalin@gmail.com>
2021-07-05 21:08:17 -0500

    add #153 js solution

---
+++ leetcodes/153. Find Minimum in Rotated Sorted Array/findMinimumInRotatedSortedArray.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var findMin = function(nums) {
+    //input : integers array
+    //output: integer
+    //edge case:
+    //constraints: nums.length >=1
+    /***
+     * test case: [3 4 5 1 2], [4 5 1 2 3 ], [ 1 2 3 4 5], [2 3 4 5 1]
+     * iterate while l < h
+     * get mid
+     * if last < first -> check right side [mid , h]
+     * if last > first ->
+     *  if mid > mid + 1 return mid + 1
+     *  if mid < mid -1 return mid
+     *  else check left side -> [l, mid -1]
+     * exit return l
+     *
+     * or
+     * if nums[h]>= nums[m] -> [l, m]
+     * else [m + 1, h]
+     * return nums[l] exit when l = h
+     *
+     *
+     */
+    let nLen = nums.length;
+    if (nLen === 1) return nums[0];
+    let h = nLen - 1;
+    let l = 0;
+    while (l < h) {
+      let mid = l + Math.floor((h - l)/2);
+      if (nums[h] >= nums[mid]) {
+        h = mid;
+      } else {
+        l = mid + 1;
+      }
+    }
+    return nums[l];
+};
