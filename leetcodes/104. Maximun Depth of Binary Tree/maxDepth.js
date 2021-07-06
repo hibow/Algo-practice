e7ad8e631c277850f66a7e98d0bf2b83c62603ca
hibow <utfnalin@gmail.com>
2021-07-05 23:24:30 -0500

    modified #104 js

--- leetcodes/104. Maximun Depth of Binary Tree/maxDepth.js
+++ leetcodes/104. Maximun Depth of Binary Tree/maxDepth.js
@@ -6,11 +6,11 @@
  * }
   DFS:
 max    0         2           3
-count  0 1   2    
+count  0 1   2
        3  -> 9  ->null
-count 0 1      2      3       
+count 0 1      2      3
         3   -> 20  -> 15  -> null
-        
+
   inital count = 0
   recursive
   check root -> if not valid -> return 0
@@ -18,8 +18,9 @@ count 0 1      2      3
                  if not , replace max
                 if valid return 1
   go down to root left  and repeat the above step with carrying count number
-  return total and add on to initial count 
-time O(n)
+  return total and add on to initial count
+time O(n) space O(n) worse case
+best or average case: space O(logN) -> O(1) for logN times
 
  */
 
