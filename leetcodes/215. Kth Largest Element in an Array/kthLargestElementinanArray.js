cdcf5ebd4ef5b81a874fb35de83c3f77b654df6b
hibow <utfnalin@gmail.com>
2021-07-07 23:36:29 -0500

    add #215 js solution

--- leetcodes/215. Kth Largest Element in an Array/kthLargestElementinanArray.js
+++ leetcodes/215. Kth Largest Element in an Array/kthLargestElementinanArray.js
@@ -1,10 +1,10 @@
-/*          
- * [Source] 
- *          
+/*
+ * [Source]
+ *
  * https://leetcode.com/problems/kth-largest-element-in-an-array/
- *          
+ *
  * [Problem Description]
- *          
+ *
  * Find the kth largest element in an unsorted array. Note that it is the kth
  * largest element in the sorted order, not the kth distinct element.
  * Example 1:
@@ -15,11 +15,29 @@
  * Output: 4
  * Note:
  * You may assume k is always valid, 1 ≤ k ≤ array's length.
- *          
+ *
  * [Comments]
- *          
- *          
- *          
- */          
+ *
+ *
+ *
+ */
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+ var findKthLargest = function(nums, k) {
+  //input: numbers array, integer
+  //output: integer
+  //edge case:
+  //quick sort timeO(n), space O(1)
+  //sort: time O(nlogn), space O(1)
+  let len = nums.length;
+  var compareNumbers = function(a, b) {
+    return a-b;
+  }
+  nums.sort(compareNumbers);
+  return nums[len-k]
 
+};
 
