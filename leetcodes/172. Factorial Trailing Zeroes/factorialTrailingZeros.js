956310c6f7b92449f9bfb28b462071f3e0fb9c59
hibow <utfnalin@gmail.com>
2021-07-08 13:10:12 -0500

    add #172 js solution

---
+++ leetcodes/172. Factorial Trailing Zeroes/factorialTrailingZeros.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+ var trailingZeroes = function(n) {
+    //input: integer
+    //output: integer
+    //edge case if n <= 1 return 0
+    /**
+     * 2 x 5 = 10, count how many 5s in n! since 2s is way more than 5s.
+     * 
+     */
+    let count = 0;
+    while(n> 0) {
+      n=Math.floor(n/5);
+      count+=n;
+    }
+    return count;
+};
