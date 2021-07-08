25840315167849cdd9f4d1853f8f622a4c2a209d
hibow <utfnalin@gmail.com>
2021-07-08 13:11:49 -0500

    add #172 comments

--- leetcodes/172. Factorial Trailing Zeroes/factorialTrailingZeros.js
+++ leetcodes/172. Factorial Trailing Zeroes/factorialTrailingZeros.js
@@ -8,7 +8,7 @@
     //edge case if n <= 1 return 0
     /**
      * 2 x 5 = 10, count how many 5s in n! since 2s is way more than 5s.
-     * 
+     * time O(log_5(N))
      */
     let count = 0;
     while(n> 0) {
