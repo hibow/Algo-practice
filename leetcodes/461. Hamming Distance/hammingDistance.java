ed7a160f8fa504f09aec353f4061cbb5211981c5
hibow <utfnalin@gmail.com>
2021-07-09 10:36:23 -0500

    add #461 java solution

---
+++ leetcodes/461. Hamming Distance/hammingDistance.java
@@ -0,0 +1,36 @@
+import jdk.nashorn.api.tree.YieldTree;
+
+class Solution {
+  // 1) XOR
+  // use xor to get the difference, and count 1s in it
+  public int hammingDistance1(int x, int y) {
+    int z = x ^ y;
+    int cnt = 0;
+    while (z != 0) {
+      // count 1s
+      if ((z & 1) == 1)
+        cnt++;
+      z = z >> 1;
+      // right shift with signed, get 0s in left side
+    }
+    return cnt;
+  }
+
+  // 2) use z & (z -1) to get rid of the rightmost 1s in z
+  public int hammingDistance2(int x, int y) {
+    int z = x ^ y;
+    int cnt = 0;
+    while (z != 0) {
+      // count 1s
+      z &= (z - 1);
+      cnt++;
+      // right shift with signed, get 0s in left side
+    }
+    return cnt;
+  }
+
+  // 3) JAVA integer.bitcount() to count all 1s
+  public int hammingDistance3(int x, int y) {
+    return Integer.bitCount(x ^ y);
+  }
+}
