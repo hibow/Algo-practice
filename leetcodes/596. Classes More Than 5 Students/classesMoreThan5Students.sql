f7c51ce626af513c1a6e780e9ca2be812d29aeee
hibow <utfnalin@gmail.com>
2021-08-03 14:25:43 -0500

    add #596 sql solution

---
+++ leetcodes/596. Classes More Than 5 Students/classesMoreThan5Students.sql
@@ -0,0 +1,34 @@
+-- There is a table courses with columns: student and class
+
+-- Please list out all classes which have more than or equal to 5 students.
+
+-- For example, the table:
+
+-- +---------+------------+
+-- | student | class      |
+-- +---------+------------+
+-- | A       | Math       |
+-- | B       | English    |
+-- | C       | Math       |
+-- | D       | Biology    |
+-- | E       | Math       |
+-- | F       | Computer   |
+-- | G       | Math       |
+-- | H       | Math       |
+-- | I       | Math       |
+-- +---------+------------+
+
+-- 1. use subquery, find the count of student by groupby
+select class
+from
+  ( select
+    class, count(distinct student) as num
+    from courses
+    group by class) as temp_table
+where num >=5;
+
+--2. use group by class with having count condition
+select class
+from courses
+group by class
+having count(distinct student) >= 5
