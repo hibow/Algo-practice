9117f93bc0eaab2088efdf4f0012311402dff420
hibow <utfnalin@gmail.com>
2021-08-03 14:28:17 -0500

    add #620 sql table

--- leetcodes/620. Not Boring Movies/notBoringMovies.sql
+++ leetcodes/620. Not Boring Movies/notBoringMovies.sql
@@ -4,7 +4,7 @@
 -- Please write a SQL query to output movies with an odd numbered ID and a description that is not 'boring'.
 -- Order the result by rating.
 
- 
+
 
 -- For example, table cinema:
 
@@ -25,6 +25,19 @@
 -- |   1     | War       |   great 3D   |   8.9     |
 -- +---------+-----------+--------------+-----------+
 
+DROP TABLE
+IF
+    EXISTS cinema;
+CREATE TABLE cinema ( id INT, movie VARCHAR ( 255 ), description VARCHAR ( 255 ), rating FLOAT ( 2, 1 ) );
+INSERT INTO cinema ( id, movie, description, rating )
+VALUES
+    ( 1, 'War', 'great 3D', 8.9 ),
+    ( 2, 'Science', 'fiction', 8.5 ),
+    ( 3, 'irish', 'boring', 6.2 ),
+    ( 4, 'Ice song', 'Fantacy', 8.6 ),
+    ( 5, 'House card', 'Interesting', 9.1 );
+
+
 select * from cinema
 where id % 2 = 1 and description not in ('boring')
-order by rating desc 
+order by rating desc
