7883e276e5dd3b0aa1ffbd813b1f606e0d5e04ad
hibow <utfnalin@gmail.com>
2021-08-05 11:17:09 -0500

    reviewed #196 sql solution

--- leetcodes/196. Delete Duplicate Emails/deleteduplicateEmails.sql
+++ leetcodes/196. Delete Duplicate Emails/deleteduplicateEmails.sql
@@ -22,12 +22,40 @@ Your output is the whole Person table after executing your sql. Use delete state
 
 # Write your MySQL query statement below
 
+#join table
+select p1.*
+from Person p1, Person p2
+where p1.Email = p2.Email;
+
+#find bigger id having same email address
+select p1.*
+from Person p1, Person p2
+Where p1.Email = p2.Email And p1.Id > p2.Id 
+
+
+#Alter to delete command -> delete p1 from p1 and p2
+
 DELETE p1 FROM Person p1, Person p2
 WHERE p1.Email = p2.Email AND p1.Id > p2.Id
 
+
+#subquery
 
 delete from Person where id not in( 
     select t.id from (
         select min(id) as id from Person group by email
     ) t
 )
+
+
+delete 
+from Person
+where 
+    id not in (
+        select id from 
+          (
+              select min(id) as id 
+              from Person
+              group by email
+          ) as m 
+    );
