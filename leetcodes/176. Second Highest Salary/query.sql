b899f75064bce61a842f530f432f6b9dd1fe5d88
hibow <utfnalin@gmail.com>
2021-08-12 23:54:12 -0500

    modified #176 sql solution

--- leetcodes/176. Second Highest Salary/query.sql
+++ leetcodes/176. Second Highest Salary/query.sql
@@ -19,7 +19,9 @@ Write a SQL query to get the second highest salary from the Employee table.
 | 2  | 200    |
 | 3  | 300    |
 +----+--------+
-For example, given the above Employee table, the query should return 200 as the second highest salary. If there is no second highest salary, then the query should return null.
+For example, given the above Employee table, the query should return 200 as the second highest salary.
+
+edge case:!!! If there is no second highest salary, then the query should return null.
 
 +---------------------+
 | SecondHighestSalary |
@@ -30,11 +32,19 @@ For example, given the above Employee table, the query should return 200 as the 
 edge case: only one record, avoid null
 */
 
-#1
+-- solution1: it will have issue with nth highest
 SELECT max(Salary) as SecondHighestSalary from Employee
 Where Salary not in (select max(Salary) from Employee)
 
-#2
+-- solution2: join
+select Max(a.Salary) as SecondHighestSalary
+From Employee a
+join Employee b
+  on a.Salary < b.Salary;
+
+
+-- Solution 3: Limit 1 Offset N-1 with IFNULL
+
 SELECT
     IFNULL(
       (SELECT DISTINCT Salary
@@ -42,3 +52,13 @@ SELECT
        ORDER BY Salary DESC
         LIMIT 1 OFFSET 1),
     NULL) AS SecondHighestSalary
+
+
+SELECT
+    (SELECT DISTINCT
+            Salary
+        FROM
+            Employee
+        ORDER BY Salary DESC
+        LIMIT 1 OFFSET 1) AS SecondHighestSalary
+;
