d7d4220d2ee20a7aad62b41422d19dcc868dd5d1
hibow <utfnalin@gmail.com>
2021-08-26 12:39:26 -0500

    add #512 sql solution

---
+++ leetcodes/512. Game Play Analysis II/gamePlayII.sql
@@ -0,0 +1,51 @@
+-- +--------------+---------+
+-- | Column Name  | Type    |
+-- +--------------+---------+
+-- | player_id    | int     |
+-- | device_id    | int     |
+-- | event_date   | date    |
+-- | games_played | int     |
+-- +--------------+---------+
+-- (player_id, event_date) is the primary key of this table.
+-- This table shows the activity of players of some game.
+-- Each row is a record of a player who logged in and played a number of games (possibly 0) before logging out on some day using some device.
+ 
+
+-- Write a SQL query that reports the device that is first logged in for each player.
+
+-- The query result format is in the following example:
+
+-- Activity table:
+-- +-----------+-----------+------------+--------------+
+-- | player_id | device_id | event_date | games_played |
+-- +-----------+-----------+------------+--------------+
+-- | 1         | 2         | 2016-03-01 | 5            |
+-- | 1         | 2         | 2016-05-02 | 6            |
+-- | 2         | 3         | 2017-06-25 | 1            |
+-- | 3         | 1         | 2016-03-02 | 0            |
+-- | 3         | 4         | 2018-07-03 | 5            |
+-- +-----------+-----------+------------+--------------+
+
+-- Result table:
+-- +-----------+-----------+
+-- | player_id | device_id |
+-- +-----------+-----------+
+-- | 1         | 2         |
+-- | 2         | 3         |
+-- | 3         | 1         |
+-- +-----------+-----------+
+
+-- solution: ranking the event_date in a table and get the first rank from player_id
+/*don't use group by*/
+/*with table is introduced from Oracle 9i release 2 database. here I use subquery instead*/
+select 
+t.player_id, t.device_id
+from 
+(select player_id, device_id, 
+  Rank() Over(partition by player_id
+              order by event_date) as rk 
+  from Activity
+) as t
+where t.rk = 1
+
+
