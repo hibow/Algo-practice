1f2649200b6955175024df8f0ab375e31adfe9c7
hibow <utfnalin@gmail.com>
2022-01-19 00:35:59 -0600

    add #11 java solution

---
+++ leetcodes/11. Container With Most Water/maxArea.java
@@ -0,0 +1,19 @@
+class Solution {
+  public int maxArea(int[] height) {
+    //constraint: len >=2
+    //input: array, output: integer
+    //two pointers while loop, time O(n), space O(1)
+    int n = height.length;
+    int s = 0;
+    int e = n - 1;
+    int max_area = 0;
+    while (s < e) {
+      int w = e - s;
+      int h = (height[s] > height[e])? height[e]: height[s];
+      max_area = Math.max(max_area, w*h);
+      if (height[s] <= height[e]) s++;
+      else e--;
+    }
+    return max_area;
+  }
+}
