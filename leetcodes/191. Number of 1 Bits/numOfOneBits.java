56785f3388d8e178951450d99b1f114589d02717
hibow <utfnalin@gmail.com>
2022-01-19 20:42:19 -0600

    modified #191 java solution

--- leetcodes/191. Number of 1 Bits/numOfOneBits.java
+++ leetcodes/191. Number of 1 Bits/numOfOneBits.java
@@ -3,6 +3,7 @@ public class Solution {
   public int hammingWeight(int n) {
     // AND & -> check the last bits if it's 1
     // >>> unsigned right shift, until n is 0
+    // run 32 times -> O(1) since integer is 32 bits
     int ones = 0;
     while(n != 0) {
       ones += (n & 1);
