c6934a237dee88ac4b0ffbbdf3f7b288fb72f8d4
hibow <utfnalin@gmail.com>
2022-05-07 21:50:26 -0500

    modify #371 py solution

--- leetcodes/371. Sum of Two Integers/sumOfTwoInteger.py
+++ leetcodes/371. Sum of Two Integers/sumOfTwoInteger.py
@@ -5,13 +5,22 @@ class Solution:
       ^ -> xor to get  0^ 1 = 1, 1 ^ 1 = 0, 0 ^ 0 = 0
       after ^ then ^ the carry number from & until no carry number
       timeO(32) -> O(1) space O(1)
+      note: Python allows unlimited length of integers.
+      use mask 0xffffffff with an (very long) integer will only keep the last 32 bits by &
+      2's complement -> reverse all bits + 1
+      (n ^ mask) + 1 = taking n's 2's complement
+      so -n = ~n + 1 = ~(n^mask + 1) + 1 = ~ (n^mask) == -n (two 1s has been canceled)
+      ~ is fliping bits
       '''
-      if a==0:
-        return b
-      if b == 0:
+      mask = 0xffffffff
+      if b == 0: #test case: -1, 0
         return a
-      while(b != 0):
-        c = a & b
-        a = a ^ b
-        b = c << 1
+      while b:
+        sum = (a ^ b) & mask
+        carry = ((a & b) << 1) & mask
+        a = sum
+        b = carry
+      #if a is negative number ex: -8 -12
+      if (a >> 31) & 1:
+        return ~(a^mask)
       return a
