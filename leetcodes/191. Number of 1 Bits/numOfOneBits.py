bd1aaafbc38c14d5f3f4c25da655a30dc9a6a50a
hibow <utfnalin@gmail.com>
2022-05-07 21:52:15 -0500

    modify #191 solution

--- leetcodes/191. Number of 1 Bits/numOfOneBits.py
+++ leetcodes/191. Number of 1 Bits/numOfOneBits.py
@@ -2,5 +2,15 @@ class Solution:
     def hammingWeight(self, n: int) -> int:
       ones = 0
       while n:
-        ones += (n&1)
-        n = n >>>1
+        n = n & (n - 1)
+        ones = ones + 1
+        #in java or js we use >>> unsigned right shift (logical right shift 1010 -> 0101; if 1010 >> 1 = 1001 )
+        #python has no unsigned right shift
+        #n & (n - 1) => able to right shift until no zeros remained
+      return ones
+
+'''
+class Solution(object):
+    def hammingWeight(self, n):
+       return bin(n).count('1')
+'''
